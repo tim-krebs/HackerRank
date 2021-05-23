fc9a8277075ff64a25503264384406f15fbcad84
tim-krebs <timkrebs9@gmail.com>
2021-05-23 11:13:12 +0200

    solved problem

--- HackerRanks/PrintPretty.cpp
+++ HackerRanks/PrintPretty.cpp
@@ -3,17 +3,28 @@
 using namespace std;
 
 int main() {
-	int T; cin >> T;
-	cout << setiosflags(ios::uppercase);
-	cout << setw(0xf) << internal;
-	while(T--) {
-		double A; cin >> A;
-		double B; cin >> B;
-		double C; cin >> C;
+  int T; cin >> T;
+  cout << setiosflags(ios::uppercase);
+  cout << setw(0xf) << internal;
+  while(T--) {
+    double A; cin >> A;
+    double B; cin >> B;
+    double C; cin >> C;
 
-		/* Enter your code here */
+    /* Enter your code here */
+        // LINE 1 
+        cout << hex << left << showbase << nouppercase; // formatting
+        cout << (long long) A << endl; // actual printed part
 
-	}
-	return 0;
+        // LINE 2
+        cout << dec << right << setw(15) << setfill('_') << showpos << fixed << setprecision(2); // formatting
+        cout << B << endl; // actual printed part
+
+        // LINE 3
+        cout << scientific << uppercase << noshowpos << setprecision(9); // formatting
+        cout << C << endl; // actual printed part
+
+  }
+  return 0;
 
 }
